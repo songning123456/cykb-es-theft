3f7730bb6039bbbfa4db913297593fa8d20b4d85
songning <1457065857@qq.com>
2020-04-16 10:24:19 +0800

    first commit

---
+++ src/main/java/com/sn/cykbestheft/CykbEsTheftApplication.java
@@ -0,0 +1,13 @@
+package com.sn.cykbestheft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CykbEsTheftApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CykbEsTheftApplication.class, args);
+    }
+
+}
